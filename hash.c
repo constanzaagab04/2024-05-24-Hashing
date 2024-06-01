cbc81d2595612004c1c10f6c8f9037be05ea6cdd
constanzaagab04 <constanzagabriel10@gmail.com>
2024-05-31 21:23:27 -0300

    Initial commit

---
+++ hash.c
@@ -0,0 +1,5 @@
+//
+// Created by Constanza Gabriel on 5/31/24.
+//
+
+#include "hash.h"
---
+++ hash.h
@@ -0,0 +1,8 @@
+//
+// Created by Constanza Gabriel on 5/31/24.
+//
+
+#ifndef CONSTANZA_HASH_HASH_H
+#define CONSTANZA_HASH_HASH_H
+
+#endif //CONSTANZA_HASH_HASH_H
