9807945efe7c8fd277e6b7e51be5b63f6f719a7b
constanzaagab04 <162723726+constanzaagab04@users.noreply.github.com>
2024-06-14 02:44:28 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -3,3 +3,27 @@
 //
 
 #include "hash.h"
+Nodo* crearNodo(int dato){
+    Nodo* nuevoNodo=(Nodo*)malloc(sizeof(Nodo));
+    if(nuevoNodo==NULL){
+        printf("No hay memoria suficiente.");
+        exit(-1);
+    }
+    nuevoNodo->dato=dato;
+    nuevoNodo->siguiente=NULL;
+    return nuevoNodo;
+}
+void insertarOrdenado(Nodo** cabeza, int dato) {
+    Nodo* nuevoNodo = crearNodo(dato);
+    if (*cabeza == NULL || (*cabeza)->dato > dato) {
+        nuevoNodo->siguiente = *cabeza;
+        *cabeza = nuevoNodo;
+    } else {
+        Nodo* actual = *cabeza;
+        while (actual->siguiente != NULL && actual->siguiente->dato < dato) {
+            actual = actual->siguiente;
+        }
+        nuevoNodo->siguiente = actual->siguiente;
+        actual->siguiente = nuevoNodo;
+    }
+}
